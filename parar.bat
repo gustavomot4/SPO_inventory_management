77b7499a5b0256e58c8279c0d96e3176691dc011
gustavomot4 <gustavo.mota@sptech.school>
2026-05-30 23:08:09 -0300

    FIX: corrigindo bugs docker

--- parar.bat
+++ parar.bat
@@ -7,7 +7,20 @@ echo  PIMENTA OUSADA - Encerrando o Sistema
 echo ============================================================
 echo.
 
-docker compose down
+:: Localizar docker (mesmo fallback do iniciar.bat)
+set DOCKER_CMD=
+where docker >nul 2>nul
+if %errorlevel% equ 0 ( set DOCKER_CMD=docker & goto rundo )
+if exist "C:\Program Files\Docker\Docker\resources\bin\docker.exe" ( set DOCKER_CMD="C:\Program Files\Docker\Docker\resources\bin\docker.exe" & goto rundo )
+if exist "%ProgramFiles%\Docker\Docker\resources\bin\docker.exe" ( set DOCKER_CMD="%ProgramFiles%\Docker\Docker\resources\bin\docker.exe" & goto rundo )
+if exist "%LOCALAPPDATA%\Docker\Docker\resources\bin\docker.exe" ( set DOCKER_CMD="%LOCALAPPDATA%\Docker\Docker\resources\bin\docker.exe" & goto rundo )
+echo [ERRO] Docker nao encontrado. Verifique o Docker Desktop.
+pause & exit /b 1
+
+:rundo
+for %%F in (%DOCKER_CMD%) do set PATH=%%~dpF;%PATH%
+
+%DOCKER_CMD% compose down
 
 if %errorlevel% neq 0 (
     echo [AVISO] Ocorreu um problema ao encerrar. Verifique o Docker Desktop.
